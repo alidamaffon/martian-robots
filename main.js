3e43ce3512cda8c1df0bb077827b13b6786985e8
ali <alida.maffon@gmail.com>
2026-06-19 08:33:44 +0100

    feat: implement robot class

--- main.js
+++ main.js
@@ -19,3 +19,73 @@ class Grid {
     return x < 0 || y < 0 || x > this.width || y > this.height;
   }
 }
+
+class Robot {
+  constructor(grid, x, y, orientation) {
+    this.grid = grid;
+    this.x = x;
+    this.y = y;
+    this.orientation = 'NESW'.indexOf(orientation) % 4;
+  }
+
+  turnLeft() {
+    this.orientation = (this.orientation + 3) % 4;
+  }
+
+  turnRight() {
+    this.orientation = (this.orientation + 1) % 4;
+  }
+
+  moveForward() {
+    // Direction vectors are: N=[0,1], E=[1,0], S=[0,-1], W=[-1,0]
+    const directions = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0],
+    ];
+    // Deltas
+    const [dx, dy] = directions[this.orientation];
+
+    if (this.grid.isOutOfBounds(this.x + dx, this.y + dy)) {
+      if (!this.grid.isScented(this.x, this.y)) {
+        this.grid.leaveScent(this.x, this.y);
+        return false; // Because robot is lost
+      }
+    } else {
+      this.x += dx;
+      this.y += dy;
+    }
+  }
+
+  applyInstructions(instruction) {
+    switch (instruction) {
+      case 'R':
+        this.turnRight();
+        break;
+      case 'L':
+        this.turnLeft();
+        break;
+      case 'F':
+        return this.moveForward();
+    }
+
+    return true;
+  }
+
+  processInstructions(instructions) {
+    for (let i = 0; i < instructions.length; i++) {
+      //Robot is lost
+      if (!this.applyInstructions(instructions.charAt(i))) return false;
+    }
+    return true;
+  }
+
+  toString() {
+    const orientations = ['N', 'E', 'S', 'W'];
+    return `${this.x} ${this.y} ${orientations[this.orientation]}`;
+  }
+}
+
+// Use both classes to build UI for the robot simulation
+
